5ae9af11c2dd7ba9f229e88ff9132c87c53eb4a7
Hussain Hammad <hussain.hammad@greeenboard.com>
2020-02-22 22:47:27 +0300

    fix signature format, add save/upload data as json

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,114 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         Email Signature Generator
       </div>
 
       <v-spacer></v-spacer>
 
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon @click="saveJson()">
+              mdi-json
+            </v-icon>
+            <a
+              ref="signatureJson"
+              :href="jsonFile"
+              target="_blank"
+              :download="fileName"
+              hidden
+            ></a>
+          </v-btn>
+        </template>
+        <span>Save data as JSON file</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon @click="$refs.file.click()">
+              mdi-file-upload
+            </v-icon>
+            <input
+              ref="file"
+              type="file"
+              accept="application/json"
+              @change="uploadFile"
+              style="display: none"
+            />
+          </v-btn>
+        </template>
+        <span>Upload data as JSON file</span>
+      </v-tooltip>
+
+      
+
+      
+
       <v-btn
         href="https://github.com/hussainhammad/email-signature-generator"
         target="_blank"
         text
       >
-        <span class="mr-2">GitHub Repo</span>
         <v-icon>mdi-github-circle</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <EmailSignatureGenerator/>
+      <EmailSignatureGenerator :item="item" />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import EmailSignatureGenerator from './components/EmailSignatureGenerator';
+  import EmailSignatureGenerator from "./components/EmailSignatureGenerator";
 
-export default {
-  name: 'App',
-
-  components: {
-    EmailSignatureGenerator,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    name: "App",
+    components: {
+      EmailSignatureGenerator
+    },
+    data: () => ({
+      item: {
+        name: null,
+        position: null,
+        department: null,
+        company: null,
+        city: null,
+        region: null,
+        country: null,
+        phone: null,
+        fax: null,
+        mobile: null,
+        email: null,
+        website: null
+      }
+    }),
+    computed: {
+      fileName() {
+        return this.item
+          ? "signature - " + this.item.name + ".json"
+          : "signature.json";
+      },
+      jsonFile() {
+        let data =
+          "data:text/json;charset=utf-8," +
+          encodeURIComponent(JSON.stringify(this.item));
+        return data;
+      }
+    },
+    methods: {
+      uploadFile(e) {
+        let file = e.target.files[0] || e.dataTransfer.files[0];
+        const reader = new FileReader();
+        reader.onload = e => (this.item = JSON.parse(e.target.result));
+        reader.readAsText(file);
+      },
+      saveJson() {
+        this.$refs.signatureJson.click();
+      }
+    }
+  };
 </script>
--- src/components/EmailSignatureGenerator.vue
+++ src/components/EmailSignatureGenerator.vue
@@ -3,95 +3,127 @@
     <v-row>
       <!-- Input -->
       <v-col>
-        <v-text-field v-model="name" label="Name"></v-text-field>
+        <v-text-field v-model="item.name" label="Name"></v-text-field>
         <v-row>
-          <v-col><v-text-field v-model="position" label="Position"></v-text-field></v-col>
-          <v-col><v-text-field v-model="department" label="Department"></v-text-field></v-col>
+          <v-col
+            ><v-text-field
+              v-model="item.position"
+              label="Position"
+            ></v-text-field
+          ></v-col>
+          <v-col
+            ><v-text-field
+              v-model="item.department"
+              label="Department"
+            ></v-text-field
+          ></v-col>
         </v-row>
-        <v-text-field v-model="company" label="Company"></v-text-field>
+        <v-text-field v-model="item.company" label="Company"></v-text-field>
         <v-row>
-          <v-col><v-text-field v-model="city" label="City"></v-text-field></v-col>
-          <v-col><v-text-field v-model="region" label="Region"></v-text-field></v-col>
+          <v-col
+            ><v-text-field v-model="item.city" label="City"></v-text-field
+          ></v-col>
+          <v-col
+            ><v-text-field v-model="item.region" label="Region"></v-text-field
+          ></v-col>
         </v-row>
-        <v-text-field v-model="country" label="Country"></v-text-field>
+        <v-text-field v-model="item.country" label="Country"></v-text-field>
         <v-row>
-          <v-col><v-text-field v-model="phone" label="Phone"></v-text-field></v-col>
-          <v-col><v-text-field v-model="fax" label="Fax"></v-text-field></v-col>
-          <v-col><v-text-field v-model="mobile" label="Mobile"></v-text-field></v-col>
+          <v-col
+            ><v-text-field v-model="item.phone" label="Phone"></v-text-field
+          ></v-col>
+          <v-col
+            ><v-text-field v-model="item.fax" label="Fax"></v-text-field
+          ></v-col>
+          <v-col
+            ><v-text-field v-model="item.mobile" label="Mobile"></v-text-field
+          ></v-col>
         </v-row>
-        <v-row>
-          <v-col></v-col>
-          <v-col></v-col>
-        </v-row>
-        <v-text-field v-model="email" label="Email"></v-text-field>
-        <v-text-field v-model="website" label="Website"></v-text-field>
+        <v-text-field v-model="item.email" label="Email"></v-text-field>
+        <v-text-field v-model="item.website" label="Website"></v-text-field>
       </v-col>
 
       <!-- New Email Signature -->
       <v-col>
-        <v-card>
-          <v-card-title>New Email</v-card-title>
-          <v-card-content>
-            <p class="px-5 pb-5">
-              <span v-if="name"><b>{{ name }}</b></span>
-              <span v-if="position">{{ " | " + position }}</span>
-              <span v-if="department">{{ " | " + department }}</span>
-              <br />
-              <span v-if="company"><b>{{ company }}</b></span>
-              <span v-if="city">{{ " | " + city }}</span>
-              <span v-if="region">{{ " | " + region }}</span>
-              <span v-if="country">{{ " | " + country }}</span>
-              <br />
-              <span v-if="phone"><b>T</b>{{ phone }}</span>
-              <span v-if="fax"><b>{{" | "}}F</b>{{ fax }}</span>
-              <span v-if="mobile"><b>{{"  |  "}}M </b>{{ mobile }}</span>
-              <br />
-              <span v-if="email"><a :href="'mailto:' + email">{{email}}</a></span>
-              <span v-if="website">{{ " | " }}<a :href="website" target="_blank">{{website}}</a></span>
-            </p>
-          </v-card-content>
-        </v-card>
-        <v-card>
-          <v-card-title>Reply/Forward</v-card-title>
-          <v-card-content>
-            <p class="px-5 pb-5">
-              <span v-if="name"><b>{{ name }}</b></span>
-              <span v-if="position">{{ " | " + position }}</span>
-              <span v-if="department">{{ " | " + department }}</span>
-              <br />
-              <span v-if="mobile"><b>M</b>{{ mobile }}</span>
-              <span v-if="email">{{ " | " }}<a :href="'mailto:' + email">{{ email}}</a></span>
-            </p>
-          </v-card-content>
-        </v-card>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>New Email</v-card-title>
+              <v-card-text>
+                <p class="px-5 pb-5">
+                  <span v-if="item.name"
+                    ><b>{{ item.name }}</b></span
+                  >
+                  <span v-if="item.position">&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.position }}</span>
+                  <span v-if="item.department">&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.department }}</span>
+                  <br />
+                  <span v-if="item.company"
+                    ><b>{{ item.company }}</b></span
+                  >
+                  <span v-if="item.city">&nbsp;&nbsp;|&nbsp;&nbsp;{{item.city }}</span>
+                  <span v-if="item.region">&nbsp;&nbsp;|&nbsp;&nbsp;{{item.region }}</span>
+                  <span v-if="item.country">&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.country }}</span>
+                  <br />
+                  <span v-if="item.phone"><b>T</b>&nbsp;{{ item.phone }}</span>
+                  <span v-if="item.fax"
+                    >&nbsp;&nbsp;|&nbsp;&nbsp;<b>F</b>&nbsp;{{ item.fax }}</span
+                  >
+                  <span v-if="item.mobile"
+                    >&nbsp;&nbsp;|&nbsp;&nbsp;<b>M</b>&nbsp;{{ item.mobile }}</span
+                  >
+                  <br />
+                  <span v-if="item.email"
+                    ><a :href="'mailto:' + item.email">{{
+                      item.email
+                    }}</a></span
+                  >
+                  <span v-if="item.website"
+                    >&nbsp;&nbsp;|&nbsp;&nbsp;<a :href="'http://' + item.website" target="_blank">{{
+                      item.website
+                    }}</a></span
+                  >
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>Reply/Forward</v-card-title>
+              <v-card-text>
+                <p class="px-5 pb-5">
+                  <span v-if="item.name"
+                    ><b>{{ item.name }}</b></span
+                  >
+                  <span v-if="item.position">&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.position }}</span>
+                  <span v-if="item.department">&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.department }}</span>
+                  <br />
+                  <span v-if="item.mobile"><b>M</b>&nbsp;{{ item.mobile }}</span>
+                  <span v-if="item.email"
+                    >&nbsp;&nbsp;|&nbsp;&nbsp;<a :href="'mailto:' + item.email">{{
+                      item.email
+                    }}</a></span
+                  >
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
 
       <!-- Reply/Forward Email Signature -->
-      <v-col>
-        
-      </v-col>
+      <v-col> </v-col>
     </v-row>
-    
   </v-container>
 </template>
 
 <script>
   export default {
     name: "EmailSignatureGenerator",
-
-    data: () => ({
-      name: null,
-      position: null,
-      department: null,
-      company: null,
-      city: null,
-      region: null,
-      country: null,
-      phone: null,
-      fax: null,
-      mobile: null,
-      email: null,
-      website: null
-    })
+    props: {
+      item: Object
+    },
+    data: () => ({})
   };
 </script>
